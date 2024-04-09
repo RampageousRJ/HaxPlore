530d0ee31044ad877e6bb95c34aa06d7f886f8a7
Rishav-RB <rishavbiswas24052002@gmail.com>
2024-04-09 14:16:39 +0530

    Initial Commit v2

--- blockchain/contracts/contractApi.sol
+++ blockchain/contracts/contractApi.sol
@@ -11,42 +11,42 @@ contract contractApi {
     Record public removeMe; 
 
     mapping (string => Record) public records;
+    Record[] public recordArray;
 
     constructor() {
         owner = msg.sender;
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Only owner can perform this action");
         _;
     }
 
-    function setRecord (string memory _bookingId,string memory _transactionId) public onlyOwner {
+    function setRecord (string memory _bookingId, string memory _transactionId) public onlyOwner {
         Record memory record = Record(_bookingId, _transactionId);
         records[_bookingId] = record; 
+        recordArray.push(record);
     }
 
     function getRecord (string memory _bookingId) public view returns (string memory) {
-        require(bytes(records[_bookingId].bookingId).length !=0 , "Record is not available");
-        Record memory record = records[_bookingId];
-        return (record.transactionId);
+        require(bytes(records[_bookingId].bookingId).length != 0, "Record is not available");
+        return records[_bookingId].transactionId;
     }
 
-    /*function getAllRecords() public view returns (Record[] memory){
-        return RecordArray;
-    }*/
-
-    /*function updateRecord(uint256 _bookingId, string memory _name, uint256 _price, uint256 _quantity) public onlyOwner {
-        require(records[_bookingId].bookingId !=0 , "Record is not available");
-        deleteRecord(_bookingId);
-        records[_bookingId] = Record(_bookingId, _name, _price, _quantity);
-        RecordArray.push(Record(_bookingId, _name, _price, _quantity));
+    function getAllRecords() public view returns (Record[] memory){
+        return recordArray;
     }
-    */
 
     function deleteRecord(string memory _bookingId) public onlyOwner {
-        require(bytes(records[_bookingId].bookingId).length !=0 , "Record is not available");
+        require(bytes(records[_bookingId].bookingId).length != 0, "Record is not available");
         delete records[_bookingId];
+        for (uint i = 0; i < recordArray.length; i++) {
+            if (keccak256(bytes(recordArray[i].bookingId)) == keccak256(bytes(_bookingId))) {
+                removeMe = recordArray[i];
+                recordArray[i] = recordArray[recordArray.length-1];
+                recordArray[recordArray.length - 1] = removeMe;
+            }
+        }
+        recordArray.pop();
     }
-
 }
